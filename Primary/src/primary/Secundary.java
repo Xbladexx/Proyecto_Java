4e7c36550a5b3b7353f0e1489e70c33c7c3872e8
inoricode28 <inori237@gmail.com>
2024-03-12 11:46:49 -0500

    Dinero

---
+++ Primary/src/primary/Secundary.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primary;
+
+/**
+ *
+ * @author Nova
+ */
+public class Secundary {
+
+    public static void main(String[] args) {
+        System.out.println("Monedas");
+        System.out.println("Billetes");
+    }
+    
+}
